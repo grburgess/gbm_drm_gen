1a8529d7df6a6e0e59a49ab61791d15f1f5499d5
grburgess <jmichaelburgess@gmail.com>
2017-06-26 15:25:44 +0200

    added reading of db

--- gbm_drm_gen/__init__.py
+++ gbm_drm_gen/__init__.py
@@ -2,13 +2,27 @@ from drmgen import DRMGen
 from drmgen_tte import DRMGenTTE
 from balrog_drm import BALROG_DRM
 
+import pkg_resources
 
 import h5py
 
-from gbm_drm_gen.config.gbm_drm_gen_config import gbm_drm_gen_config
+
+def get_path_of_data_file(data_file):
+    file_path = pkg_resources.resource_filename("gbm_drm_gen", 'data/%s' % data_file)
+
+    return file_path
 
 
-__database = h5py.File('balrog_db.h5','r')
+
+
+
+path_to_balrog_db = get_path_of_data_file('balrog_db.h5')
+
+
+#from gbm_drm_gen.config.gbm_drm_gen_config import gbm_drm_gen_config
+
+
+__database = h5py.File(path_to_balrog_db,'r')
 
 
 
