e520f244336acad4e9a7424be671f25ad5f57761
grburgess <jmichaelburgess@gmail.com>
2017-06-26 15:05:54 +0200

    import of database

--- gbm_drm_gen/__init__.py
+++ gbm_drm_gen/__init__.py
@@ -3,4 +3,12 @@ from drmgen_tte import DRMGenTTE
 from balrog_drm import BALROG_DRM
 
 
+import h5py
+
 from gbm_drm_gen.config.gbm_drm_gen_config import gbm_drm_gen_config
+
+
+__database = h5py.File('balrog_db.h5','r')
+
+
+
