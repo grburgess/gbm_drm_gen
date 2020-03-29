5f3cea7fde337d6a907a6efadcae2fdd235b045f
grburgess <jmichaelburgess@gmail.com>
2020-03-29 11:07:10 +0200

    switch version system

--- gbm_drm_gen/__init__.py
+++ gbm_drm_gen/__init__.py
@@ -23,3 +23,7 @@ __all__ = [
     "BALROGHealpixMap",
     "Palantir",
 ]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
-from setuptools import find_packages, Command # Extension
 from numpy.distutils.core import Extension # as Numpy_Extension
 from numpy.distutils.core import setup
 import os
 import io
-import sys
-from shutil import rmtree
 import numpy
+import versioneer
 
 ext1 = Extension(
     name='ftran',
@@ -23,85 +21,6 @@ ext2 = Extension(name='at_scat',
 
 extensions = cythonize([ext1, ext2])
 
-#extensions_cython = [ext2]
-
-# Package meta-data.
-NAME = 'gbm_drm_gen'
-DESCRIPTION = 'A DRM generator for the Fermi GBM detectors'
-URL = 'https://github.com/mpe-grb/gbm_drm_gen'
-EMAIL = 'jmichaelburgess@gmail.com'
-AUTHOR = 'J. Michael Burgess'
-REQUIRES_PYTHON = '>=2.7.0'
-VERSION = None
-
-REQUIRED = [
-    'numpy',
-    'astropy',
-    'scipy',
-    'h5py',
-    'responsum',
-    #    'threeML',
-    'gbmgeometry'
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
-            sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine...')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags...')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
 
 
 # Create list of data files
@@ -120,33 +39,12 @@ def find_data_files(directory):
 
 extra_files = find_data_files('gbm_drm_gen/data')
 
-# setup(
-
-#     ext_modules=cythonize(extensions_cython), requires=['numpy']
-# )
 
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=('tests', )),
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    package_data={
-        '': extra_files,
-    },
-    license='BSD',
-
     ext_modules=extensions,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    include_package_data=True,
+    package_data={"": extra_files},
 
-    cmdclass={
-        'upload': UploadCommand,
-    },
 )
