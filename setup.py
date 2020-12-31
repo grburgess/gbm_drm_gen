a9ed3309547c244d3f70bafea2bc99c6da1c6e68
grburgess <jmichaelburgess@gmail.com>
2020-12-31 10:15:05 +0100

    and again

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ import versioneer
 
 
 
-extra_files = ["gbm_drm_gen/data/balrog_db.h5"]
+#extra_files = ["gbm_drm_gen/data/balrog_db.h5"]
 
 setup(
     version=versioneer.get_version(),
-   # include_package_data=True,
-    package_data={"": extra_files},
+    include_package_data=True,
+
     cmdclass=versioneer.get_cmdclass(),
 )
