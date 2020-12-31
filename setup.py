8a0b814a943100ab25a958fd806efcf957323a69
grburgess <jmichaelburgess@gmail.com>
2020-12-31 10:08:03 +0100

    try this

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extra_files = ["gbm_drm_gen/data/balrog_db.h5"]
 
 setup(
     version=versioneer.get_version(),
-    include_package_data=True,
+   # include_package_data=True,
     package_data={"": extra_files},
     cmdclass=versioneer.get_cmdclass(),
 )
