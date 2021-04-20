a2f399d60906dc26effd5f23d8d849ea2b44a5a6
grburgess <jmichaelburgess@gmail.com>
2021-04-20 14:25:23 +0200

    format

--- gbm_drm_gen/test/test_drm_gen.py
+++ gbm_drm_gen/test/test_drm_gen.py
@@ -1,6 +1,7 @@
 import os
-import numpy as np
+
 import astropy.io.fits as fits
+import numpy as np
 
 from gbm_drm_gen import BgoTTEEdges, DRMGenTTE, NaiTTEEdges
 from gbm_drm_gen.utils.package_data import get_path_of_data_file
@@ -28,17 +29,16 @@ def test_various_options(built_drm_gen):
 def test_custom_edges():
 
     tte_file = get_path_of_data_file(
-            "example_data/glg_tte_n6_bn110721200_v00.fit")
+        "example_data/glg_tte_n6_bn110721200_v00.fit")
 
     trigdat_file = get_path_of_data_file(
         "example_data/glg_trigdat_all_bn110721200_v01.fit"
     )
 
-    cspec_file = get_path_of_data_file("example_data/glg_cspec_n6_bn110721200_v00.pha")
+    cspec_file = get_path_of_data_file(
+        "example_data/glg_cspec_n6_bn110721200_v00.pha")
 
     edges = NaiTTEEdges.from_log_bins(145)
-    
-    n6 = DRMGenTTE(tte_file, trigdat=trigdat_file, mat_type=2, cspecfile=cspec_file, custom_input_edges=edges)
 
-
-    
+    n6 = DRMGenTTE(tte_file, trigdat=trigdat_file, mat_type=2,
+                   cspecfile=cspec_file, custom_input_edges=edges)
